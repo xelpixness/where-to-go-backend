4f33e23ac8814f33a0d9a54398b5ad2f9dfd5659
xelpixness <xelpixness@gmail.com>
2025-10-23 15:23:48 +0700

    feat: Complete map functionality with dynamic data
    - Replace static JSON with dynamic data from DB
    - Add API endpoint for place details with images and descriptions
    - Fix coordinate positioning using demo coordinates
    - Resolve Vue/Django template conflict with custom delimiters
    - Full integration of database data with frontend map

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
 
+from places.models import Place
+import json
+
 
 def home_page(request):
-    return render(request, "pages/index.html")
+    places = Place.objects.all()
+
+    geo_json = {
+        "type": "FeatureCollection",
+        "features": [
+            {
+                "type": "Feature",
+                "geometry": {"type": "Point", "coordinates": [place.lng, place.lat]},
+                "properties": {
+                    "title": place.title,
+                    "placeId": place.id,
+                    "detailsUrl": f"/place/{place.id}/",
+                },
+            }
+            for place in places
+        ],
+    }
+
+    return render(request, "pages/index.html", {"geo_json": json.dumps(geo_json)})
--- places/views.py
+++ places/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Place
 
-# Create your views here.
+
+def place_details(request, place_id):
+    place = Place.objects.prefetch_related("images").get(id=place_id)
+
+    data = {
+        "title": place.title,
+        "imgs": [img.image.url for img in place.images.all()],
+        "description_short": place.description_short,
+        "description_long": place.description_long,
+        "coordinates": {"lng": place.lng, "lat": place.lat},
+    }
+
+    return JsonResponse(data)
